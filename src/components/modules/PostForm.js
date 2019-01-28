d5cfd71a2f7f74e20b9d17ba20b9feb6926c8144
rimsan <mohamed.rimsan@viewqwest.com>
2019-01-28 10:04:59 +0800

    insert data using API

--- src/components/modules/PostForm.js
+++ src/components/modules/PostForm.js
@@ -2,13 +2,35 @@ import React from 'react';
 
 class PostForm extends React.Component {
 
+    constructor() {
+        super()
+    }
+
+    submitHander = e => {
+        e.preventDefault();
+        const body = {
+            title: e.target.title.value,
+            body: e.target.body.value
+        }
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        })
+            .then(res => res.json())
+            .then(response => {
+                console.log(response)
+            })
+    }
 
     render() {
         return (
             <div>
                 <h3>Add Post</h3>
                 <hr />
-                <form>
+                <form onSubmit={this.submitHander}>
                     <input type="text" name="title" placeholder="title" />
                     <input type="text" name="body" placeholder="body" />
                     <button type="submit">Add</button>
