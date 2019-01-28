33150fc1a2b5ee3f33c4a46e19e82ac06ab822e7
rimsan <mohamed.rimsan@viewqwest.com>
2019-01-28 09:42:41 +0800

    Displaying data from API

--- src/components/modules/Posts.js
+++ src/components/modules/Posts.js
@@ -2,10 +2,19 @@ import React from 'react';
 
 class Posts extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: []
+        }
+    }
+
     componentDidMount() {
 
         fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json()).then(response => {
-            console.log(response)
+            this.setState({
+                posts: response
+            })
         })
     }
 
@@ -13,6 +22,12 @@ class Posts extends React.Component {
         return (
             <div>
                 <h3>Posts</h3>
+                <hr />
+                <ul>
+                    {this.state.posts.map(list => (
+                        <li key={list.id}>{list.body}</li>
+                    ))}
+                </ul>
             </div>
         )
     }
